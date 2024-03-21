fab121cad873e95042b20e204db444b3fb7dd561
Tyler <ty.nelson1137@gmail.com>
2024-03-21 13:38:27 -0500

    sign-in error view

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -46,7 +46,7 @@ export default function NavBar(): ReactElement {
                                 <div ref={avaDropRef} className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                                     <div className="px-4 py-3">
                                         <span className="block text-sm text-gray-900 dark:text-white">{session?.user.displayName}</span>
-                                        <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{session?.user.email}</span>
+                                        <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{session?.user.email}</span>
                                     </div>
                                     <ul className="py-2" aria-labelledby="user-menu-button">
                                         <li>
